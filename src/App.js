d7692c850ef90e62ee91120ad383178b93bf2ba9
Abdullah Shafiq <59536208+abdullahshafiq377@users.noreply.github.com>
2022-11-30 00:31:53 +0500

    Application Completed

--- src/App.js
+++ src/App.js
@@ -1,25 +1,33 @@
-import logo from './logo.svg';
 import './App.css';
+import AddPostForm from './features/posts/AddPostForm';
+import PostsList from './features/posts/PostsList';
+import PostPage from './features/posts/PostPage';
+import Layout from './components/Layout';
+import { Routes, Route } from 'react-router-dom';
+import EditPostForm from './features/posts/EditPostForm';
+import UsersList from './features/users/UsersList';
+import UserPage from './features/users/UserPage';
+import NotFoundPage from './components/NotFoundPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<Routes>
+			<Route path='/' element={<Layout />}>
+				<Route index element={<PostsList />} />
+
+				<Route path='post'>
+					<Route index element={<AddPostForm />} />
+					<Route path=':postId' element={<PostPage />} />
+					<Route path='edit/:postId' element={<EditPostForm />} />
+				</Route>
+				<Route path='user'>
+					<Route index element={<UsersList />} />
+					<Route path=':userId' element={<UserPage />} />
+				</Route>
+				<Route path='*' element={<NotFoundPage />} />
+			</Route>
+		</Routes>
+	);
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,12 +3,30 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import store from './store';
+import { Provider } from 'react-redux';
+import { fetchUsers } from './features/users/usersSlice';
+
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { fetchPosts } from './features/posts/postsSlice';
+
+store.dispatch(fetchPosts());
+store.dispatch(fetchUsers());
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+	<React.StrictMode>
+		<Provider store={store}>
+			<Router>
+				<Routes>
+					<Route
+						path='/*'
+						element={<App />}
+					/>
+				</Routes>
+			</Router>
+		</Provider>
+	</React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
